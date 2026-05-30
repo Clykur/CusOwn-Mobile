7e8971fe2ad73b560555e01a6bc0a239663512dd
Karthik Naramala <naramalakarthik143@gmail.com>
2026-05-30 18:17:56 +0530

    Refactor of hardcoded styles

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 process.env.EXPO_PUBLIC_SUPABASE_URL = 'https://mock-url.supabase.co';
 process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY = 'mock-anon-key';
+
+if (typeof process.env.EXPO_OS === 'undefined') {
+  process.env.EXPO_OS = 'ios';
+}
+
+if (typeof global.WebSocket === 'undefined') {
+  global.WebSocket = require('ws');
+}
